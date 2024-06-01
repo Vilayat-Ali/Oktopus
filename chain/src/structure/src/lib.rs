3a70826acdee73414d0ae009731addf4b752b4c5
vilayat-ali <vilayatcodemysite@gmail.com>
2024-06-01 18:32:34 +0530

    Completed and tested code for big number addition logic

--- chain/src/structure/src/lib.rs
+++ chain/src/structure/src/lib.rs
@@ -19,6 +19,14 @@ impl std::fmt::Debug for BigNum {
     }
 }
 
+impl std::fmt::Display for BigNum {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result { 
+        write!(f, "{}.{}", 
+        self.int_val.iter().map(|x| x.to_string()).collect::<String>(), 
+        self.frac_val.iter().map(|x| x.to_string()).collect::<String>())
+    }
+}
+
 impl Into<String> for BigNum {
     fn into(self) -> String {
         format!("{}.{}",
@@ -122,20 +130,47 @@ impl BigNum {
 
             self.frac_val[idx] = digit;
         }
-    }
 
-    pub fn substract(&mut self, big_num: &BigNum) {}
+        let size = self.int_val.len().max(big_num.int_val.len());
+        let self_int_vec_size = self.int_val.len();
+
+        for idx in 1..=size {
+            let digit_1 = match self_int_vec_size >= idx {
+                true => self.int_val[self_int_vec_size - idx],
+                false => 0
+            };
+            let digit_2 = match big_num.int_val.len() >= idx {
+                true => big_num.int_val[big_num.int_val.len() - idx],
+                false => 0
+            };
+
+            let mut digit_sum = digit_1 + digit_2 + (is_carry as u8);
+            is_carry = false;
+
+            if digit_sum > 9 {
+                digit_sum = digit_sum % 10;
+                is_carry = true;
+            }
+            
+            match self_int_vec_size >= idx {
+                true => self.int_val[self_int_vec_size - idx] = digit_sum,
+                false => self.int_val.insert(0, digit_sum)
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn big_number_addition() {
-        assert_eq!(2, 1+1);
-    }
+    use crate::BigNum;
 
     #[test]
-    fn big_number_substraction() {
-        assert_eq!(0, 1-1);
+    fn big_number_addition() {
+        let mut big_num_1 = BigNum::from("12345.6789");
+        let big_num_2 = BigNum::from("0.6789");
+        
+        big_num_1.add(&big_num_2);
+
+        assert_eq!(big_num_1.to_string().as_str(), "12346.3578");
     }
 }
